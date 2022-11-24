b797b7d6486b01995828f8eaa5ecca032fcc56a4
Hyulya Gyuler <hyulya.gyuler@zigzag.global>
2022-11-24 10:53:41 +0200

    Adding the bool type excercise

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
-﻿// Creating a calculator with addition, subtraction, multiplication, division
-
+﻿
 namespace Calculator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Task 1: Creating a calculator with addition, subtraction, multiplication, division
+
             Console.WriteLine("Enter the first number: ");
             double firstNumber = Convert.ToDouble(Console.ReadLine()); // ReadLine reads strings so we need to convert the string to a double
             Console.WriteLine("Enter the second number: ");
@@ -22,6 +23,30 @@ namespace Calculator
 
             // Division
             Console.WriteLine("Division result: " + (firstNumber / secondNumber));
+
+            // Task 2: Using the bool data type with If statement and make a check with isTrue parameter.
+
+            bool isTrue = true;
+
+            if (!isTrue) // == is a logical opeation, = is the maths equal operation
+            {
+                Console.WriteLine("This is true!");
+            }
+            else 
+            {
+                Console.WriteLine("This is false!");
+            }
+
+            isTrue = false;
+
+            if (!isTrue)
+            {
+                Console.WriteLine("This is true!");
+            }
+            else
+            {
+                Console.WriteLine("This is false!");
+            }
         }
 
     }
